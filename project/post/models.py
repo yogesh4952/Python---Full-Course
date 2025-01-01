cce8e494ee4b540ce5e1ead743afe0318dd6c091
Yogesh Shah <yogeshshah2063@Gmail.com>
2025-01-01 20:33:31 +0545

    SOme edit

--- project/post/models.py
+++ project/post/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here
+class Author(models.Model):
+  first_name = models.CharField(max_length=50)
+  last_name = models.CharField(max_length=50)
+  data_of_birth = models.DateField()
+  phone_number = models.BigIntegerField()
+  email_address = models.EmailField()
+  
+  def __str__(self):
+    return self.first_name + f"  {self.id}"
+
+
+
